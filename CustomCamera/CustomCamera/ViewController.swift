ab7124c2ace37787db55b945031824d0ca7b43e4
stantrujillo <stan@rezio.net>
2017-04-03 12:00:13 +0200

    heavy re-work, started on timer...

--- CustomCamera/CustomCamera/ViewController.swift
+++ CustomCamera/CustomCamera/ViewController.swift
@@ -11,91 +11,106 @@ import AVFoundation
 
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var imgOverlay: UIImageView!
-    @IBOutlet weak var btnCapture: UIButton!
 
-    let captureSession = AVCaptureSession()
-    let stillImageOutput = AVCaptureStillImageOutput()
-    var previewLayer : AVCaptureVideoPreviewLayer?
-    
-    // If we find a device we'll store it here for later use
-    var captureDevice : AVCaptureDevice?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        captureSession.sessionPreset = AVCaptureSessionPresetHigh
-        
-        if let devices = AVCaptureDevice.devices() as? [AVCaptureDevice] {
-            // Loop through all the capture devices on this phone
-            for device in devices {
-                // Make sure this particular device supports video
-                if (device.hasMediaType(AVMediaTypeVideo)) {
-                    // Finally check the position and confirm we've got the back camera
-                    if(device.position == AVCaptureDevicePosition.back) {
-                        captureDevice = device
-                        if captureDevice != nil {
-                            print("Capture device found")
-                            beginSession()
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    @IBAction func actionCameraCapture(_ sender: AnyObject) {        
-        print("Camera button pressed")
-        saveToCamera()
-    }
-    
-    func beginSession() {
-        
-        do {
-            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-            stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
-            
-            if captureSession.canAddOutput(stillImageOutput) {
-                captureSession.addOutput(stillImageOutput)
-            }
-            
-        }
-        catch {
-            print("error: \(error.localizedDescription)")
-        }
-        
-        guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else {
-            print("no preview layer")
-            return
-        }
-        
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer.frame = self.view.layer.frame
-        captureSession.startRunning()
-        
-        self.view.addSubview(navigationBar)
-        self.view.addSubview(imgOverlay)
-        self.view.addSubview(btnCapture)
-    }
-    
-    func saveToCamera() {
-        
-        if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
-            
-            stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
-                if let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(CMSampleBuffer) {
-                    
-                    if let cameraImage = UIImage(data: imageData) {
-                        
-                        UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)                        
-                    }
-                }
-            })
-        }
-    }
+	@IBOutlet weak var imgOverlay: UIImageView!
+	@IBOutlet weak var btnCapture: UIButton!
 
+	let captureSession = AVCaptureSession()
+	let stillImageOutput = AVCaptureStillImageOutput()
+	var previewLayer : AVCaptureVideoPreviewLayer?
+
+	var captureDevice : AVCaptureDevice?
+
+	fileprivate var timer: Timer!
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+
+//		captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+
+		for device in AVCaptureDevice.devices() as! [AVCaptureDevice]
+		{
+			if (device.position == AVCaptureDevicePosition.back)
+			{
+				print("self.view.frame: \(self.view.frame)")
+				captureDevice = device
+				beginSession()
+
+			}
+		}
+	}
+
+	override func viewDidAppear(_ animated: Bool)
+	{
+		timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: fireTimer)
+	}
+
+	override func viewDidDisappear(_ animated: Bool)
+	{
+		timer?.invalidate()
+	}
+
+	fileprivate func fireTimer(timer: Timer)
+	{
+		print("tick")
+	}
+
+	@IBAction func actionCameraCapture(_ sender: AnyObject)
+	{
+		print("Camera button pressed")
+		saveToCamera()
+	}
+
+	func beginSession()
+	{
+		do
+		{
+			try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+
+			stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+
+			if captureSession.canAddOutput(stillImageOutput)
+			{
+				captureSession.addOutput(stillImageOutput)
+			}
+		}
+		catch
+		{
+			print("error: \(error.localizedDescription)")
+		}
+
+		guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else
+		{
+			print("no preview layer")
+			return
+		}
+
+		self.view.layer.addSublayer(previewLayer)
+		print("self.view.layer.frame: \(self.view.layer.frame)")
+		let frame = CGRect(x: 0, y: 20, width: self.view.layer.frame.width, height: self.view.layer.frame.height - 40)
+		previewLayer.frame = frame
+		captureSession.startRunning()
+
+		self.view.addSubview(imgOverlay)
+		self.view.addSubview(btnCapture)
+	}
+
+	func saveToCamera()
+	{
+		if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo)
+		{
+			stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
+				if let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(CMSampleBuffer)
+				{
+					if let cameraImage = UIImage(data: imageData)
+					{
+						UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)
+					}
+				}
+			})
+		}
+	}
 }
+
 
